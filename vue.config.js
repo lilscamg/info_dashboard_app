74cf9c9fa6d51d15211d298585700badb2efb852
Bulat <khafizov.dds@yandex.ru>
2024-05-17 22:11:42 +0300

    vue config js changes

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require('@vue/cli-service');
 const Dotenv = require('dotenv-webpack');
 
 module.exports = defineConfig({
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/info-dashboard-app/'
+    : '/',
   transpileDependencies: true,
   configureWebpack: {
     plugins: [
